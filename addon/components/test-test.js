96e02cd37b35c1e78522481230b804b73136190c
Konrad Zajaczkowski <konradwdgz26@gmail.com>
2017-02-16 09:55:49 -0600

    CLEANING: removed test component

--- addon/components/test-test.js
+++
@@ -1,6 +0,0 @@
-import Ember from 'ember';
-import layout from '../templates/components/test-test';
-
-export default Ember.Component.extend({
-  layout
-});
--- app/components/test-test.js
+++
@@ -1,1 +0,0 @@
-export { default } from 'emberfire-account/components/test-test';
