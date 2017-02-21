0ad1add91da23be99bc131c13c3cdddeebc62f19
Mathew Vacha <matvacha@groupwelldone.com>
2017-02-21 16:12:26 -0600

    add messages

--- app/instance-initializers/route-initializer.js
+++ app/instance-initializers/route-initializer.js
@@ -11,7 +11,18 @@ const DEFAULT_CONFIG = {
       'account.delete': 'Delete'
     },
     messages: {
-
+      successful_login: 'You have logged in successfully!',
+      unsuccessful_login: 'You were unable to log in.',
+      successful_update_account: 'You have successfully updated your account information!',
+      unsuccessful_update_account: 'We were unable to update your account information.',
+      successful_update_email: 'You have successfully updated your email!',
+      unsuccessful_update_email: 'We were unable to update your email.',
+      successful_update_password: 'You have successfully updated your password!',
+      usuccessful_update_password: 'We were unable to update your password.',
+      successful_delete_account: 'You have successfully deleted your account!',
+      unsuccessful_delete_account: 'We were unable to delete your account.',
+      successful_logout: 'You are now logged out!',
+      unsuccessful_logout: 'We were unable to log out of your account.'
     }
 };
 
