44046e5c141c8ca4a96e0cf4f16ad666d4d50471
Konrad Zajaczkowski <konradwdgz26@gmail.com>
2017-02-22 17:09:24 -0600

    FIX: improvement to verify email

--- addon/router.js
+++ addon/router.js
@@ -3,11 +3,7 @@ export default function(router) {
     this.route('delete');
     this.route('email');
     this.route('password');
-<<<<<<< HEAD
     this.route('verify-email');
-=======
-    this.route('delete');
     this.route('portal', {path: '/'});
->>>>>>> c73d6fc96d78382fb690158b4cf4aeed9ddf4a32
   });
 }
