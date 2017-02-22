ec8a1294e4a2842a4b2eb3d424d26354c1378f09
Mathew Vacha <matvacha@groupwelldone.com>
2017-02-22 10:42:19 -0600

    add sign in link if not signed in

--- addon/routes/account.js
+++ addon/routes/account.js
@@ -1,10 +1,16 @@
 import Ember from 'ember';
 
+
 const {
   Route,
   inject: { service }
 } = Ember;
 
 export default Route.extend({
-  session: service()
+  session: service(),
+  'account-config': Ember.inject.service(),
+  beforeModel() {
+    if (this.get('account-config').signInLink)
+      this.get('router').transitionTo(Object.keys(this.get('account-config').signInLink)[0]);
+  }
 });
