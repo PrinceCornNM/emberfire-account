c7da52fc154671057f1160ca9ff1697089d62108
Julian Hyde <jkhyde86@gmail.com>
2017-02-24 14:52:28 -0600

    Tests for Account-Menu Component It doesn't do much so there aren't many tests.

--- tests/integration/components/account-menu-test.js
+++ tests/integration/components/account-menu-test.js
@@ -1,12 +1,22 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
+import Ember from 'ember';
+
+const configStub = Ember.Service.extend({
+  links: {
+    'account.test': 'Test Link'
+  }
+});
 
 moduleForComponent('account-menu', 'Integration | Component | account menu', {
-  integration: true
+  integration: true,
+  beforeEach(){
+    this.register('service:account-config', configStub);
+  }
 });
 
 test('it renders', function(assert) {
   this.render(hbs`{{#account-menu}}{{/account-menu}}`);
 
-  assert.equal(this.$().text().trim(), "");
+  assert.equal(this.$().text().trim().substring(0, 9), "Test Link");
 });
