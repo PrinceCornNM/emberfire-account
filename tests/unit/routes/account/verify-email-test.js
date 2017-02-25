ad7db71f158c9aa87a5daf66e6c30104ca9b4067
Julian Hyde <jkhyde86@gmail.com>
2017-02-25 10:29:30 -0600

    TMWEB-473: Moved verify e-mail to a component

--- tests/unit/routes/account/verify-email-test.js
+++ tests/unit/routes/account/verify-email-test.js
@@ -1,20 +1,61 @@
 import { moduleFor, test } from 'ember-qunit';
 import Ember from 'ember';
 
-// This extremely simple stub is sufficient for the verify-email's init function but not really for anything else
 const firebaseStub = Ember.Service.extend({
-	auth(){
-		return true;
-	}
+  auth(){
+    let email = this.get('emailVerified');
+    let shouldFail = this.get('shouldFail');
+    return  {
+      currentUser: {
+        emailVerified: email,
+        sendEmailVerification(){
+          return new Ember.RSVP.Promise(function(resolve, reject){
+            if(shouldFail){
+              reject({code: "ERROR"});
+            }else{
+              resolve();
+            }
+          });
+        }
+      }
+    };
+  },
+  emailVerified: false,
+  shouldFail: false
 });
 
+// const notifyStub = Ember.Service.extend({
+//   lastMessage: "",
+//   alert(message){
+//     this.set("lastMessage", message);
+//   },
+//   info(message){
+//     this.set("lastMessage", message);
+//   }
+// });
+
 moduleFor('route:account/verify-email', 'Unit | Route | account/verify email', {
+  unit: true,
   beforeEach: function(){
-  	this.register('service:firebaseApp', firebaseStub);
-  }
+    this.register('service:firebaseApp', firebaseStub);
+    // this.register('service:notify', notifyStub);
+  },
+  needs: ['service:notify', 'service:firebaseApp', 'service:account-config']
 });
 
 test('it exists', function(assert) {
   let route = this.subject();
   assert.ok(route);
+});
+
+test('Should send e-mail', function(assert){
+  let route = this.subject();
+  let promise = route.send("sendEmailVerification");
+
+  promise.then(() => {
+    assert.ok(true);
+  }, () =>{
+    assert.ok(false);
+  });
+  // assert.equal(route.get('notify.lastMessage'), 'Email verification was sent, please check your inbox');
 });
