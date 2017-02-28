45d8683544917ba1220daea29f6d438bbf726bbb
Mathew Vacha <matvacha@groupwelldone.com>
2017-02-28 14:43:26 -0600

    add firebase import to index

--- index.js
+++ index.js
@@ -6,4 +6,30 @@ module.exports = {
 	isDevelopingAddon: function() {
 		return true;
 	},
+	included: function (app) {
+    this._super.included.apply(this, arguments);
+    app.import('bower_components/firebase/firebase.js');
+
+    // if (isFastBoot()) {
+    //   this.importFastBootDependencies(host);
+    // } else {
+    //   this.importBrowserDependencies(host);
+    // }
+  },
+
+  // treeForVendor: function(vendorTree) {
+  //   var trees = [];
+
+  //   if (vendorTree) {
+  //     trees.push(vendorTree);
+  //   }
+
+  //   if (isFastBoot()) {
+  //     trees.push(funnel(path.join(__dirname, './assets'), {
+  //       files: ['firebase.js']
+  //     }));
+  //   }
+
+    // return mergeTrees(trees);
+  // }
 };
