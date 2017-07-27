c8184a1ef17baee58ff540631a819346aefed9f6
Viktor Koves <viktorwdgk25@gmail.com>
2017-07-27 12:47:23 -0500

    Fixed eslint for account-menu

--- addon/components/account-menu.js
+++ addon/components/account-menu.js
@@ -1,13 +1,16 @@
 import Ember from 'ember';
 import layout from '../templates/components/account-menu';
 
-export default Ember.Component.extend({
-  layout,
+const { Component, inject, computed, get } = Ember;
+
+export default Component.extend({
+  'account-config': inject.service(),
   classNames: ['menu-section'],
-  active:    'active account-btn',
+  active: 'active account-btn',
   notActive: 'account-btn',
-  'account-config': Ember.inject.service(),
-  links: Ember.computed('account-config.links', function() {
-    return this.get('account-config.links');
+  layout,
+
+  links: computed('account-config.links', function() {
+    return get(this, 'account-config.links');
   })
 });
