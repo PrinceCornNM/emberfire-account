031dbd35f1bac9b26a0e7a80e280a5beddeb8a80
Viktor Koves <viktorwdgk25@gmail.com>
2017-07-27 14:14:57 -0500

    Fixed eslint on router

--- addon/router.js
+++ addon/router.js
@@ -1,6 +1,6 @@
 export default function(router) {
   router.route('account', function() {
-    this.route('portal', {path: '/'});
+    this.route('portal', { path: '/' });
     this.route('email');
     this.route('delete');
     this.route('password');
